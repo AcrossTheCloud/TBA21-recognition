2534cf7d00df0e88986519088381c829389e06fd
Mehrdad Amirghasemi <mehrdad.amirghasemi@gmail.com>
2019-04-01 15:43:27 +1100

    rekogniton service ready for testing

---
+++ image_tagger.js
@@ -0,0 +1,49 @@
+const AWS = require('aws-sdk');
+
+const rekognition = new AWS.Rekognition();
+
+const headers = {
+  "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+  "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+};
+
+
+
+exports.handler = async (event,context,callback) => {
+
+  console.log('Invoking image tagger in Ireland...');
+  console.log(event.queryStringParameters);
+
+  try {
+
+      let params = {
+        Image: {
+          S3Object: {
+            Bucket: event.queryStringParameters.bucketname,
+            Name: event.queryStringParameters.decodedsrckey
+          }
+        },
+        MaxLabels: 10,
+        MinConfidence: 60
+      };
+
+      let rekognitionData = await rekognition.detectLabels(params).promise();
+
+        const response = {
+          statusCode: 200,
+          headers: headers,
+          body: JSON.stringify(rekognitionData.Labels),
+        };
+        callback(null, response);
+      
+  } catch (err) {
+    console.log(err);
+    const response = {
+      statusCode: 503,
+      headers: headers,
+      body: JSON.stringify({ "message": "Server error " + error.toString() })
+    };
+    callback(null, response);  
+  }
+}
+
